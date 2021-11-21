d0a7533f6d7b84cde4d653bcdc7bb3616ddb9438
Namju Yun <southpoley@gmail.com>
2021-11-21 16:46:34 +0900

    Initial commit of the completed (for now) page

---
+++ slider.js
@@ -0,0 +1,13 @@
+const productCarousel = tns({
+  container: ".my-slider",
+  controls: true,
+  nav: false,
+  prevButton: ".slider-left",
+  nextButton: ".slider-right",
+  slideBy: "page",
+  autoplay: false,
+  autoplayHoverPause: true,
+  autoplayButtonOutput: false,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+});
